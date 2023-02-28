dc713be2b7751ea8fa376c0756ad30bd6b07f10f
Alvaro González <113747463+AlvaroGonzalezz@users.noreply.github.com>
2023-02-28 10:21:39 -0600

    Add files via upload

---
+++ POO_U2_Alvaro/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_U2_Alvaro
+{
+    internal class Class1
+    {
+
+        public void colorCasa() {
+
+            // Miembros de una Clase
+            // Declarar todos los campos necesarios para definir un objeto de tipo casa
+
+            public String nombreDueno;
+
+
+        
+        }
+    }
+}
---
+++ POO_U2_Alvaro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO_U2_Alvaro
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+
+
+            Class1 objectoCasa1= new Class1();
+
+
+            // Usar objeto
+
+            objectoCasa1.nombreDueno = "Alvaro";
+
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
