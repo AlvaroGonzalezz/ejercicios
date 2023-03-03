f59b222810129d00e05976bf2b0eb02f3d3f7f2f
Alvaro González <113747463+AlvaroGonzalezz@users.noreply.github.com>
2023-03-02 19:46:33 -0600

    Add files via upload

---
+++ POOU2_Console/Casa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOU2_Console
+{
+     class Casa
+    {
+        // Miembros de una clase 
+        // Campos: Son las variables que usaremos y se crean dentro de la clase
+        public string materialCasa, nombreDuenoCasa, apellidoDuenoCasa, ubicacionCasa, metrosCuadradosCasa;
+        double precioVentaCasa;
+        byte numHabitacionesCasa, numBañosCasa, numPuertasCasa;
+        ConsoleColor colorCasa;
+
+        // Propiedades: Cuida a una variable y guarda informacion en un campo
+      
+        public byte NumBañosCasa
+        {
+            get { return numBañosCasa; }
+            set { numBañosCasa = value; }
+        }
+
+        // Tarea: Crear una propiedad para colorCasa y precioVentaCasa
+
+        public ConsoleColor ColorCasa
+        {
+            get { return colorCasa; }
+            set { colorCasa = value;}
+        }
+
+        public double PrecioVentaCasa
+        {
+            get { return precioVentaCasa; }
+            set { precioVentaCasa = value; }
+        }
+    }
+}
---
+++ POOU2_Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOU2_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una instancia
+            Casa objetoCasa1 = new Casa();
+            Casa objetoCasa2 = new Casa();
+            Casa objetoCasa3 = new Casa();
+
+
+            // Usamos el objeto creado
+            objetoCasa1.nombreDuenoCasa = "Alvaro";
+
+            // Usamos la propiedad que protege a un campo
+            objetoCasa1.PrecioVentaCasa = 1120000;
+
+
+            
+
+        }
+    }
+}
