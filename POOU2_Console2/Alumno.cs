9cacfaec302bfb8d695c65e04d5f90dc4aca612e
Alvaro González <113747463+AlvaroGonzalezz@users.noreply.github.com>
2023-03-19 11:31:44 -0600

    Add files via upload

---
+++ POOU2_Console2/Alumno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOU2_Console2
+{
+     class Alumno
+    {
+        // Campos 
+
+        // Detalles escolares
+        string matriculaAlumno, nombreCarreraAlumno, semestreAlumno, fichaAlumno, nombreBachilleratoAlumno, correoInstitucionalAlumno;
+        DateTime fechaIngresoAlumno;
+
+        // Detalles generales del alumno
+        string  nombreAlumno, primerApellidoAlumno, segundoApellidoAlumno, curpAlumno, numeroCelularAlumno, correoPersonalAlumno;
+        string  diaNacimientoAlumno, mesNacimientoAlumno, añoNacimientoAlumno;
+
+        // Detalles de la direccion del alumno
+        string calleAlumno, numCasaAlumno, coloniaAlumno, ciudadAlumno, cpAlumno, estadoAlumno, paisAlumno;
+
+        // Detalles medicos del alumno
+        string nssAlumno;
+        char grupoSanguineoAlumno, rhAlumno;
+
+    }
+}
