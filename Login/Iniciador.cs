66ba78a5017e5c44d015d5c8efd0c25d1279d9cc
Alvaro González <113747463+AlvaroGonzalezz@users.noreply.github.com>
2023-03-30 22:10:43 -0600

    Add files via upload

---
+++ Login/Iniciador.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Login
+{
+     class Iniciador
+    {
+        // Campos
+        string nombreUsuario, contraseñaUsuario, confirmarContraseñaUsuario;
+
+
+        // Metodos
+        public void Registrar()
+        {
+            Console.WriteLine("--------REGISTRO-----------");
+            Console.WriteLine("Ingresa tu nombre de usuario a registrar: ");
+            nombreUsuario = Console.ReadLine();
+            Console.WriteLine("Crea una contraseña: ");
+            contraseñaUsuario= Console.ReadLine();
+            Console.WriteLine("Repite de nuevo la contraseña: ");
+            confirmarContraseñaUsuario= Console.ReadLine();
+            if (contraseñaUsuario == confirmarContraseñaUsuario)
+            {
+                Console.WriteLine("Registro exitoso");
+                // Iniciamos sesion
+                
+                
+                
+               
+            }
+            else
+            {
+                Console.WriteLine("Las contraseñas no coinciden");
+                Thread.Sleep(1000);
+                Console.WriteLine("Las contraseñas no coinciden");
+                Thread.Sleep(1000);
+                Environment.Exit(0); // cerrar consola
+            }
+        }
+
+        public void IniciarSesion()
+        {
+            string usuario, contraseña;
+
+            Console.WriteLine("-----------INICIAR SESIÓN---------------");
+            Console.WriteLine("Ingresa el usuario: ");
+            usuario= Console.ReadLine();
+            Console.WriteLine("Ingresa tu contraseña: ");
+            contraseña = Console.ReadLine();
+            bool iniciar = usuario == nombreUsuario && contraseña == contraseñaUsuario;
+
+            if (iniciar)
+            {
+                Console.WriteLine("Inicio exitoso.");
+            }
+            else
+            {
+                Console.WriteLine("La contraseña o el usuario estan mal escritos o no existen.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Login/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Iniciador iniciador= new Iniciador();
+            iniciador.Registrar();
+            iniciador.IniciarSesion();
+            Console.ReadKey();
+        }
+    }
+}
